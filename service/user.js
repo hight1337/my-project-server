c8ac81cce3a144893f641da66578225494351544
vlad_Chaikovskyi <hight1337@gmail.com>
2023-05-07 04:10:49 +0300

    create user services

--- service/user.js
+++ service/user.js
@@ -29,13 +29,43 @@ class UserService {
     return { ...tokens, user: userDto };
   }
 
-  async signIn(email, password) {}
+  async signIn(email, password) {
+    const user = await UserModel.findOne({ email });
+    if (!user) {
+      throw ApiError.BadRequest("Пользователь с таким email не найден");
+    }
+    const isPassEquals = await bcrypt.compare(password, user.password);
+    if (!isPassEquals) {
+      throw ApiError.BadRequest("Неверный пароль");
+    }
+    const userDto = new UserDto(user);
+    const tokens = tokenService.generateTokens({ ...userDto });
 
-  async signOut() {}
+    await tokenService.saveToken(userDto.id, tokens.refreshToken);
+    return { ...tokens, user: userDto };
+  }
 
-  async activate(activationLink) {}
+  async signOut() {
+    const token = await tokenService.removeToken(refreshToken);
+    return token;
+  }
 
-  async refresh(refreshToken) {}
+  async refresh(refreshToken) {
+    if (!refreshToken) {
+      throw ApiError.UnauthorizedError();
+    }
+    const userData = tokenService.validateRefreshToken(refreshToken);
+    const tokenFromDb = await tokenService.findToken(refreshToken);
+    if (!userData || !tokenFromDb) {
+      throw ApiError.UnauthorizedError();
+    }
+    const user = await UserModel.findById(userData.id);
+    const userDto = new UserDto(user);
+    const tokens = tokenService.generateTokens({ ...userDto });
+
+    await tokenService.saveToken(userDto.id, tokens.refreshToken);
+    return { ...tokens, user: userDto };
+  }
 }
 
 module.exports = new UserService();
